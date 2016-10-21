ece8826a0229319538a70610d583f68415addef2
Nestor Alvarez <grimreaper1169@gmail.com>
2016-10-21 13:22:51 -0700

    Add several other error handlers for our registration.

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,11 +1,19 @@
 helpers do
 
   def registration_errors(params)
+    email_regex = /^[a-zA-Z0-9]+.?[a-zA-Z0-9]*@[a-zA-Z0-9]*.?[a-zA-Z0-9]*.[a-zA-Z]{2,}.?[a-zA-Z]{2,}$/
+    username_regex = /^[a-zA-Z0-9]{6,}$/
 
     if User.find_by(email: params[:user]['email'])
       return "That e-mail is already taken."
     elsif User.find_by(username: params[:user]['username'])
       return "That username is already taken."
+    elsif !email_regex.match(params[:user]['email'])
+      return "Invalid email. Please try again."
+    elsif params[:user]['username'].length < 7
+      return "Username must be at least 6 characters."
+    elsif !username_regex.match(params[:user]['username'])
+      return "Invalid username. Alphanumeric characters only."
     end
 
   end
