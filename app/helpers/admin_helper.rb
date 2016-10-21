ae039c50cfa337dfffc7a3fe48ccec7e828c321b
Nestor J. Alvarez <grimreaper1169@gmail.com>
2016-10-21 00:28:59 -0700

    Add a sort_db helper method for our tired old admin who needs to see the best scores. Among other things.

---
+++ app/helpers/admin_helper.rb
@@ -0,0 +1,21 @@
+helpers do
+
+  def sort_db
+
+    elements = Assessment.all
+
+    elements = elements.select { |element| !element.score.nil? }
+    sorted_results = elements.sort_by { |item| -item.score }
+
+    sorted_results.each do |res|
+      user = User.find(res.id)
+      p user.username.to_s + ' got a ' + res.score.to_s
+      p user.email.to_s
+      p "*" * 20
+    end
+
+    sorted_results
+
+  end
+
+end
