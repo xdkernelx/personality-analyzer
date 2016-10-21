10656d0c45bee96bfb62045b578e19f3f197a3f3
Nestor Alvarez <grimreaper1169@gmail.com>
2016-10-21 11:07:37 -0700

    Fix improper field symbol call in our validates.

--- app/models/score.rb
+++ app/models/score.rb
@@ -1,5 +1,5 @@
 class Score < ActiveRecord::Base
   validates :email, :score, { presence: true }
-  validates :email, username: { uniqueness: true }
+  validates :email, :username, { uniqueness: true }
   validates :email, format: { with: /^[a-zA-Z0-9]+.?[a-zA-Z0-9]*@[a-zA-Z0-9]*.?[a-zA-Z0-9]*.[a-zA-Z]{2,}.?[a-zA-Z]{2,}/, multiline: true }
 end
