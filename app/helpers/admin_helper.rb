eeffd0b7fe4fa1421aedc803a11596af4021410b
Nestor J. Alvarez <grimreaper1169@gmail.com>
2016-10-21 01:10:21 -0700

    Comment out the helper method.

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -1,21 +1,21 @@
-helpers do
+# helpers do
 
-  def sort_db
+#   def sort_db
 
-    elements = Assessment.all
+#     elements = Assessment.all
 
-    elements = elements.select { |element| !element.score.nil? }
-    sorted_results = elements.sort_by { |item| -item.score }
+#     elements = elements.select { |element| !element.score.nil? }
+#     sorted_results = elements.sort_by { |item| -item.score }
 
-    sorted_results.each do |res|
-      user = User.find(res.id)
-      p user.username.to_s + ' got a ' + res.score.to_s
-      p user.email.to_s
-      p "*" * 20
-    end
+#     sorted_results.each do |res|
+#       user = User.find(res.id)
+#       p user.username.to_s + ' got a ' + res.score.to_s
+#       p user.email.to_s
+#       p "*" * 20
+#     end
 
-    sorted_results
+#     sorted_results
 
-  end
+#   end
 
-end
+# end
