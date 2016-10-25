26a4dd4c9ebd376cd571464824bc602a15a05f0d
Nestor Alvarez <grimreaper1169@gmail.com>
2016-10-25 14:23:04 -0700

    Deleted the Score model and migration.

--- app/models/score.rb
+++
@@ -1,5 +0,0 @@
-class Score < ActiveRecord::Base
-  validates :email, :score, { presence: true }
-  validates :email, { uniqueness: true }
-  validates :email, format: { with: /^[a-zA-Z0-9]+.?[a-zA-Z0-9]*@[a-zA-Z0-9]*.?[a-zA-Z0-9]*.[a-zA-Z]{2,}.?[a-zA-Z]{2,}/, multiline: true }
-end
